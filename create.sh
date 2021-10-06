82649ec7edb63ed13311e6440d56a38857cbea84
Restyled.io <commits@restyled.io>
2021-10-06 14:57:45 +0000

    Restyled by shfmt

--- create.sh
+++ create.sh
@@ -9,14 +9,14 @@ set -x
 export ORG_DOMAIN="${ORG_DOMAIN:-cluster.local}"
 
 port=6440
-for cluster in source target ; do
-    if k3d cluster get "$cluster" >/dev/null 2>&1 ; then
-        echo "Already exists: $cluster" >&2
-    else
-        k3d cluster create "$cluster" \
-            --api-port="$((port++))" \
-            --network=multicluster-example \
-            --k3s-server-arg="--cluster-domain=$cluster.$ORG_DOMAIN" \
-            --wait
-    fi
+for cluster in source target; do
+  if k3d cluster get "$cluster" >/dev/null 2>&1; then
+    echo "Already exists: $cluster" >&2
+  else
+    k3d cluster create "$cluster" \
+      --api-port="$((port++))" \
+      --network=multicluster-example \
+      --k3s-server-arg="--cluster-domain=$cluster.$ORG_DOMAIN" \
+      --wait
+  fi
 done
--- link.sh
+++ link.sh
@@ -5,23 +5,22 @@ set -eu
 ORG_DOMAIN="${ORG_DOMAIN:-cluster.local}"
 LINKERD="${LINKERD:-linkerd}"
 
-
 # Generate credentials so the service-mirror
 #
 # Unfortunately, the credentials have the API server IP as addressed from
 # localhost and not the docker network, so we have to patch that up.
 fetch_credentials() {
-    cluster="$1"
-    # Grab the LB IP of cluster's API server & replace it in the secret blob:
-    lb_ip=$(kubectl --context="k3d-$cluster" get svc -n kube-system traefik \
-        -o 'go-template={{ (index .status.loadBalancer.ingress 0).ip }}')
-    
-    # shellcheck disable=SC2001  
-    echo "$("$LINKERD" --context="k3d-$cluster" \
-            multicluster link --set "enableHeadlessServices=true" \
-            --cluster-name="$cluster" \
-            --log-level="debug" \
-            --api-server-address="https://$lb_ip:6443")" 
+  cluster="$1"
+  # Grab the LB IP of cluster's API server & replace it in the secret blob:
+  lb_ip=$(kubectl --context="k3d-$cluster" get svc -n kube-system traefik \
+    -o 'go-template={{ (index .status.loadBalancer.ingress 0).ip }}')
+
+  # shellcheck disable=SC2001
+  echo "$("$LINKERD" --context="k3d-$cluster" \
+    multicluster link --set "enableHeadlessServices=true" \
+    --cluster-name="$cluster" \
+    --log-level="debug" \
+    --api-server-address="https://$lb_ip:6443")"
 }
 
 # East (source) & West (target) get access to each other.
@@ -30,6 +29,6 @@ fetch_credentials source | kubectl --context=k3d-target apply -n linkerd-multicl
 fetch_credentials target | kubectl --context=k3d-source apply -n linkerd-multicluster -f -
 
 sleep 10
-for c in source target ; do
-    "$LINKERD" --context="k3d-$c" mc check
+for c in source target; do
+  "$LINKERD" --context="k3d-$c" mc check
 done
